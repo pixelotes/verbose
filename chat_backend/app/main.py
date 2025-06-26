60566543b6040900b5082c28d0d50d6e38ec44d9
pixelotes <donpastor@gmail.com>
2025-06-26 09:08:53 +0200

    Update main.py

--- chat_backend/app/main.py
+++ chat_backend/app/main.py
@@ -5,6 +5,7 @@ from app.routes.chat import router as chat_router
 app = FastAPI()
 app.include_router(chat_router)
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Update for prod
